e7914e8c0c944cad241ca982b7e9dc3b3dfe1d73
ssaqui <anacarnales@icloud.com>
2024-10-23 15:07:29 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -9,6 +10,6 @@
  */
 	int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
