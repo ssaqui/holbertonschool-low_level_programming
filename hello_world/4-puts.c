e0830780485c37a770ea1ad84457efb0cbadf205
ssaqui <anacarnales@icloud.com>
2024-10-23 15:02:47 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
-int main(void)
-{	/* En vez de usar printf usar puts */
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints the string
+ * "Programming is like building a multilingual puzzle" using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+	int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
