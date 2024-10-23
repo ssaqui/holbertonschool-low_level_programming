e154ac6fba01bc5dd1f80e9834ab9fa74817aa8f
ssaqui <anacarnales@icloud.com>
2024-10-23 14:47:52 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,8 @@
-gcc
--o
-puts "Programming is like building a multilingual puzzle"
+#include <stdio.h>
+
+int main(void)
+{ /* En vez de usar printf usar puts */
+
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
