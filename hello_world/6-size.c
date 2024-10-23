055c0bd218392e8d5fc8ef33332347b969368743
ssaqui <anacarnales@icloud.com>
2024-10-23 16:04:56 +0000

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print the size of differents data
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d", sizeof(char));
+	printf("Size of an int: %d", sizeof(int));
+	printf("Size of a long int: %d", sizeof(int));
+	printf("Size of a long long int: %d", sizeof(int));
+	printf("Size of a float: %d", sizeof(float));
+	return (0);
+}
